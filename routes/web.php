51ed24822e6cccd8bc68674688c92289300e3a18
Wagura Maurice <wagura465@gmail.com>
2020-12-02 14:26:39 +0300

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/company', CompanyController::class);
